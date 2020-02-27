2b38e399d61a72322230d889536ace2427a5ae04
Raymond Zhong <raykyri@gmail.com>
2020-02-26 17:33:06 -0800

    Add packages, add Edgeware mainnet network ID, and add public key to address converter

--- client/app.js
+++ client/app.js
@@ -3,20 +3,23 @@ import 'styles.css';
 
 import * as $ from 'jquery';
 import { mnemonicGenerate, mnemonicValidate } from '@polkadot/util-crypto';
-import { u8aToHex } from '@polkadot/util';
+import { u8aToHex, hexToU8a } from '@polkadot/util';
 import Keyring from '@polkadot/keyring';
+import createPair from '@polkadot/keyring/pair';
 
 
 $('.generate-keypair').click((e) => {
   const phrase = mnemonicGenerate();
-  const keyring = new Keyring({ type: 'sr25519' });
-  const keyringPair = keyring.addFromMnemonic(phrase);
+  const testnetKeyring = new Keyring({ type: 'sr25519' });
+  const testnetKeyringPair = testnetKeyring.addFromMnemonic(phrase);
+  const mainnetKeyring = new Keyring({ type: 'sr25519', ss58Format: 7 });
+  const mainnetKeyringPair = mainnetKeyring.addFromMnemonic(phrase);
 
   $('.generated-account').show();
   $('.generated-phrase').text(phrase);
-  $('.generated-publickey').text('' + u8aToHex(keyringPair.publicKey()));
-  $('.generated-testnet-ss58').text(keyringPair.address());
-  $('.generated-edgeware-ss58').text(keyringPair.address());
+  $('.generated-publickey').text('' + u8aToHex(testnetKeyringPair.publicKey));
+  $('.generated-testnet-ss58').text(testnetKeyringPair.address);
+  $('.generated-mainnet-ss58').text(mainnetKeyringPair.address);
 });
 
 $('.ss58-convert').click((e) => {
@@ -34,4 +37,25 @@ $('.ss58-convert').click((e) => {
   $('.converted-account').show();
   $('.converted-address').text(address);
   $('.converted-publickey').text(publickey);
+});
+
+$('.pk-convert').click((e) => {
+  let testnetAddress;
+  let mainnetAddress;
+  const publickey = '' + $('.pk-publickey').val();
+  try {
+    const testnetKeyring = new Keyring({ type: 'sr25519' });
+    const testnetPair = createPair({ toSS58: testnetKeyring.encodeAddress, type: 'sr25519' }, { publicKey: hexToU8a(publickey) });
+    testnetAddress = testnetPair.address;
+    const mainnetKeyring = new Keyring({ type: 'sr25519', ss58Format: 7 });
+    const mainnetPair = createPair({ toSS58: mainnetKeyring.encodeAddress, type: 'sr25519' }, { publicKey: hexToU8a(publickey) });
+    mainnetAddress = mainnetPair.address;
+  } catch (e) {
+    alert('Invalid public key!');
+    return;
+  }
+  $('.pk-converted-account').show();
+  $('.pk-converted-publickey').text(publickey);
+  $('.pk-converted-testnet-address').text(testnetAddress);
+  $('.pk-converted-mainnet-address').text(mainnetAddress);
 });
